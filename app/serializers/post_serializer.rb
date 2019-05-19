3f1d072eae3fbad5ec87c6ef0f7335fc922e8bde
LeTanThanh <tanthanhcr95@gmail.com>
2019-05-19 15:11:28 +0700

    add_PostSerializer::CommentSerializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,4 +2,8 @@ class PostSerializer < ApplicationSerializer
   attributes :id, :title, :body
 
   has_many :comments
+
+  class CommentSerializer < ApplicationSerializer
+    attributes :id, :name
+  end
 end
